3c10fc194c141f69bfaf915b8cb90cfe2da481cd
gRenka <128580487+gRenka-with-40k-garlic@users.noreply.github.com>
2023-10-29 15:10:36 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WinFormsAlarmApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 24);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Завести будильник";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.maskedTextBox2.Location = new System.Drawing.Point(72, 64);
+            this.maskedTextBox2.Mask = "00:00";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(89, 45);
+            this.maskedTextBox2.TabIndex = 7;
+            this.maskedTextBox2.ValidatingType = typeof(System.DateTime);
+            this.maskedTextBox2.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox2_MaskInputRejected);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(133, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.label2.Location = new System.Drawing.Point(179, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 46);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "00:00";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 154);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Reflection.Emit;
+
+namespace WinFormsAlarmApp
+{
+    public partial class Form1 : Form
+    {
+        Timer timer01 = new Timer();
+        SoundPlayer sp = new SoundPlayer("P:\\gennady_documents\\Coding tools\\VS-projects(C)\\WinFormsAlarmApp\\Alarm.wav");
+        bool b = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            timer01.Interval = 1000;
+            timer01.Tick += new EventHandler(timer1_Tick);
+            timer01.Start();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (b == false)
+            {
+                label2.Text = maskedTextBox2.Text;
+                timer2.Start();
+                maskedTextBox2.Visible = false;
+                button1.Text = "Убрать будильник";
+                b = true;
+            }
+            else if (b == true)
+            {
+                label2.Text = "00:00";
+                timer2.Stop();
+                maskedTextBox2.Visible = true;
+                button1.Text = "Завести будильник";
+                b = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sp.Stop();
+            button2.Enabled = false;
+            maskedTextBox2.Visible = true;
+            button1.Text = "Завести будильник";
+            b = false;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.Hour.ToString("00") + ":" + DateTime.Now.Minute.ToString("00") + ":" + DateTime.Now.Second.ToString("00");
+        }
+
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (label1.Text == label2.Text + ":00")
+            {
+                button2.Enabled = true;
+                sp.Play();
+            }
+        }
+    }
+}
